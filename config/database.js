4166a9a8e544acca47b4b71aaface09367048752
Hungvu <vonglaucac2@gmail.com>
2023-07-11 22:19:04 +0700

    update5

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose')
+//DB_URI = "mongodb://127.0.0.1/Ecommerce"
+const mongoose = require("mongoose");
 
-const connectDatabase =()=>{
-    mongoose.connect(process.env.DB_URI).then((data)=>{
-        console.log(`Mongodb connected with server ${data.connection.host}`);
-       });
-}
+const connectDatabase = () => {
+  mongoose.connect(process.env.DB_URI).then((data) => {
+    console.log(`Mongodb connected with server ${data.connection.host}`);
+  });
+};
 
-module.exports = connectDatabase
+module.exports = connectDatabase;
